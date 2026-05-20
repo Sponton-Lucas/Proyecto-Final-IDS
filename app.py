afee2d8c4cc008c26baaf8f35769ec538df8c316
José Toro <tororivasjoseantonio@gmail.com>
2026-05-20 09:04:58 -0300

    elimino para unir a los otros

--- app.py
+++
@@ -1,8 +0,0 @@
-@app.route('/resenas/<int:id>', methods=['DELETE'])
-def delete_resena(id):
-    borrado = db.delete_resena(id)
-    if borrado:
-        return jsonify({'message': 'Reseña eliminada'}), 200
-    else:
-        return jsonify({'error': 'Reseña no encontrada'}), 404
-
