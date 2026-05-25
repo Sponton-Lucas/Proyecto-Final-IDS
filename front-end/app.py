f9b7d024dff6c53e8e647c558d6f604bce069817
Lucas <lsponton@fi.uba.ar>
2026-05-25 19:21:23 -0300

    Agregado de los templates a utilizar de momento, y creacion del navbar en el base.html sin estilizado aún. #31 #132

--- front-end/app.py
+++ front-end/app.py
@@ -1,13 +1,35 @@
 from flask import Flask, jsonify, render_template
-import requests
 
 app = Flask(__name__)
 
-@app.route('/usuarios', methods=['GET'])
-def get_usuarios():
-    response = requests.get('http://localhost:5000/usuarios')
-    usuarios = response.json()
-    return render_template('index.html', usuarios=usuarios)
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+@app.route("/menu")
+def menu():
+	return render_template('menu.html')
+
+@app.route("/conocenos")
+def conocenos():
+	return render_template('conocenos.html')
+
+@app.route("/resenas")
+def resenas():
+	return render_template('resenas.html')
+
+@app.route("/reservas")
+def reservas():
+	return render_template('reservas.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route("/registro")
+def registro():
+	return render_template('registro.html')
+
 
 if __name__ == '__main__':
 	app.run(port=3000, debug=True)  
