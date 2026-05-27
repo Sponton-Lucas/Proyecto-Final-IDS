8151362a105fe218dc8a99dca01ec217e01ea502
AgustinAntonic <aantonic@fi.uba.ar>
2026-05-27 15:06:57 -0300

    Se realizan los issues #148 #152 #129

--- front-end/app.py
+++ front-end/app.py
@@ -1,17 +1,10 @@
-from flask import Flask, jsonify, render_template, request, redirect
-import requests
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    res = requests.get('http://localhost:5000/resenas')
-    resenas = res.json()
-    us = requests.get('http://localhost:5000/usuarios')
-    usuarios = us.json()
-    ser = requests.get('http://localhost:5000/servicios_extra')
-    servicios_extra = ser.json()
-    return render_template('index.html', re=resenas, u=usuarios, se=servicios_extra)
+	return render_template('index.html')
 
 @app.route("/menu")
 def menu():
@@ -21,30 +14,13 @@ def menu():
 def conocenos():
 	return render_template('conocenos.html')
 
-@app.route("/resenas", methods=['GET'])
+@app.route("/resenas")
 def resenas():
-    res = requests.get('http://localhost:5000/resenas')
-    resenas = res.json()
-    us = requests.get('http://localhost:5000/usuarios')
-    usuarios = us.json()
-    return render_template('resenas.html', re=resenas, u=usuarios)
-
-#a revisar, porque para crear una resena tiene que estar logeado
-@app.route('/agregar_resena', methods=['POST'])
-def agregar_alumno():
-    requests.post("http://localhost:5000/resenas", data=request.form)
-    return redirect('/resenas')
-#a revisar, porque para crear una resena tiene que estar logeado
-
+	return render_template('resenas.html')
 
 @app.route("/reservas")
 def reservas():
 	return render_template('reservas.html')
-
-@app.route('/registrarse', methods=['POST'])
-def login_form():
-    requests.post("http://localhost:5000/usuarios", json=request.form.to_dict())
-    return redirect('/')
 
 @app.route('/login')
 def login():
