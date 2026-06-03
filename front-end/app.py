f2300aa6478cf2b29925f591043e48fa4681ee11
inolasco52 <inolasco@fi.uba.ar>
2026-06-03 11:15:02 -0300

    Agregado de conceccion de formulario de reservas con el backend, incluyendo lo de necesitar estar logeado para hacer una reserva

--- front-end/app.py
+++ front-end/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, redirect, render_template, request, session
+import requests
 
 app = Flask(__name__)
 
@@ -18,8 +19,19 @@ def conocenos():
 def resenas():
 	return render_template('resenas.html')
 
-@app.route("/reservas")
+@app.route('/reservas', methods=['GET', 'POST'])
 def reservas():
+	if 'usuario_id' not in session:
+		return redirect('/login')
+	if request.method == 'POST':
+		datos = {
+			"usuario_id": session.get('usuario_id'),
+			"fecha": request.form.get('fecha'),
+			"hora": request.form.get('horario'),
+			"cantidad_personas": int(request.form.get('personas'))
+		}
+		requests.post("http://localhost:5000/reservas", json=datos)
+		return redirect('/') 
 	return render_template('reservas.html')
 
 @app.route('/login')
