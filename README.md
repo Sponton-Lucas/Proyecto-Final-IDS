b3ccedd9e47675fabbe8054c71a5d5e8db018d02
Lucas <lsponton@fi.uba.ar>
2026-06-17 01:16:50 -0300

    Se actualiza el reuirements en el back y front, se modifica el readme, y se borra funcion en el app.py del back q estaba de mas.

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
 # ¡Proyecto-Final-IDS!
+# Restaurante App
 
-# Database Setup
 
-1. Se actualizo init.sql para crear la base de datos y el usuario siguiendo los siguientes pasos:
-    a) Instalar MySQL
-    b) Ejecutar: mysql -u root -p < init.sql
-    c) Ingresar la contraseña root de MySQL
-    d) Ejecutar la app
+## 🚀 Instalación rápida
+
+### 1. Clonar el repositorio
+```bash
+git clone <URL_DEL_REPO>
+cd <NOMBRE_DEL_REPO>
+```
+
+### 2. Crear la base de datos
+
+mysql -u <usuario_mysql> -p < back-end/sql/init.sql
+( Reemplazá <usuario_mysql> por tu usuario de MySQL (ej: root o el que creaste).)
+
+### 3. Crear usuario de mySql
+
+CREATE USER IF NOT EXISTS 'caidaSiu'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON restaurante_db.* TO 'caidaSiu'@'localhost';
+FLUSH PRIVILEGES;
+
+### 4. Inicializar entornos virtuales
+./init.sh
+(Esto crea los entornos en back-end/venv y front-end/venv e instala dependencias.)
+
+### 5. Levantar el back-end
+cd back-end
+source venv/bin/activate
+flask run
+
+### 6. Levantar el front-end
+cd front-end
+source venv/bin/activate
+flask run
--- back-end/sql/init.sql
+++ back-end/sql/init.sql
@@ -59,23 +59,6 @@ CREATE TABLE IF NOT EXISTS reservas (
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
 );
 
-ALTER TABLE postres ADD COLUMN descripcion VARCHAR(200);
-ALTER TABLE bebidas ADD COLUMN descripcion VARCHAR(200);
-ALTER TABLE comida_principal ADD COLUMN descripcion VARCHAR(200);
+INSERT INTO usuarios (id_usuario, nombre_apellido, email, telefono, contrasenia, es_admin)
+VALUES (1, 'Admin', 'admin@gmail.com', '1234567890', '$2b$12$S.q7K65PcCVxJ3sztawZ6.8uOlwhHrhABAIGdpvFUXs1rBbu26aha', TRUE);
 
-ALTER TABLE  comida_principal ADD COLUMN imagen VARCHAR(350);
-ALTER TABLE bebidas ADD COLUMN imagen VARCHAR(350);
-ALTER TABLE postres ADD COLUMN imagen VARCHAR(350);
-
-INSERT INTO usuarios (id_usuario, nombre_apellido, email, telefono, contrasenia, es_admin) #se inserta el super admin predeterminado
-VALUES (1, 'Admin', 'admin@gmail.com', '1234567890', '1234', TRUE);
-UPDATE usuarios SET contrasenia = '$2b$12$S.q7K65PcCVxJ3sztawZ6.8uOlwhHrhABAIGdpvFUXs1rBbu26aha' WHERE id_usuario = 1; #contraseña hasheada (externamente mediante bcrypt) del super admin
-
-#USUARIO MYSQL
-CREATE USER IF NOT EXISTS'caidaSiu'@'localhost'
-IDENTIFIED BY '1234';
-
-GRANT ALL PRIVILEGES
-ON restaurante_db.*
-TO 'caidaSiu'@'localhost';
-FLUSH PRIVILEGES;
